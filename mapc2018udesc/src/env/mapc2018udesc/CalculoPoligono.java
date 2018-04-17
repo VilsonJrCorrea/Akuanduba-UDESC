bdb736db4ae6cddf7f580b4b41b28430a43d5393
TiagoFunkUdescCeavi <tiagoff.tf@gmail.com>
2018-04-17 14:50:38 -0300

    Vou subir o servidor

---
+++ mapc2018udesc/src/env/mapc2018udesc/CalculoPoligono.java
@@ -0,0 +1,14 @@
+// CArtAgO artifact code for project mapc2018udesc
+
+package mapc2018udesc;
+
+import cartago.*;
+
+public class CalculoPoligono extends Artifact {
+
+	@OPERATION
+	void teste() {
+		
+	}
+}
+
